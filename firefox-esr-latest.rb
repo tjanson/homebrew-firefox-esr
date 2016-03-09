2d4e3a6bee242558d5319bf6b9e06451866df5b3
Tom Janson <tom.janson@rwth-aachen.de>
2016-03-09 22:27:12 +0100

    Add Firefox ESR 45 (45.0) as latest

---
+++ firefox-esr-latest.rb
@@ -0,0 +1,16 @@
+cask 'firefox-esr-latest' do
+  version '45.0'
+  sha256 '390984d3922d4a3101f50a7a03cdb0b33b8cdd58dff7324405efb9ad7441cf19'
+
+  url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/en-US/Firefox%20#{version}esr.dmg"
+  name 'Mozilla Firefox'
+  homepage 'https://www.mozilla.org/en-US/firefox/organizations/'
+  license :mpl
+
+  app 'Firefox.app'
+
+  zap delete: [
+                '~/Library/Application Support/Firefox',
+                '~/Library/Caches/Firefox',
+              ]
+end
