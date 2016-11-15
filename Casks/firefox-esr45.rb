1f1818af8118f572660ee747ba6673568205eec5
Tom Janson <tom.janson@rwth-aachen.de>
2016-11-15 20:32:43 +0100

    Update firefox-esr45 to 45.5.0

--- Casks/firefox-esr45.rb
+++ Casks/firefox-esr45.rb
@@ -1,10 +1,12 @@
 cask 'firefox-esr45' do
-  version '45.4.0'
-  sha256 '4e40d588b838709822bd3ccc1edbd13f055df8daf73d1024bcc6c9cbe07ef1e9'
+  version '45.5.0'
+  sha256 'd6b9f8c0a722b2ddb2138b13c5964d77cf29052f0553024e16ff391d2f5328ef'
 
+  # mozilla.net was verified as official when first introduced to the cask
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/en-US/Firefox%20#{version}esr.dmg"
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/en-US/firefox/organizations/'
+
   license :mpl
 
   app 'Firefox.app'
