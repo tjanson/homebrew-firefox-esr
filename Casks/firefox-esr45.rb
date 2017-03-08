147e2d541ae2f85270f2c2f136c36d8fdd651bec
Tom Janson <tom.janson@rwth-aachen.de>
2017-03-08 14:08:33 +0100

    Update Firefox ESR 45 (45.8.0 ESR)

--- Casks/firefox-esr45.rb
+++ Casks/firefox-esr45.rb
@@ -1,6 +1,6 @@
 cask 'firefox-esr45' do
-  version '45.7.0'
-  sha256 '36c3049d8c0e265e2ba3a10e57ce09b99f3aef3fac110ec32f17706e8ee5cd77'
+  version '45.8.0'
+  sha256 '0552674271a8bbaa3b4fc9c137ee286cbdb74eeda1f1536d02faaeb1e4e25162'
 
   # mozilla.net was verified as official when first introduced to the cask
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/en-US/Firefox%20#{version}esr.dmg"
