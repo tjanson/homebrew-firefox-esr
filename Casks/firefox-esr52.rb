73afdad747ef2306f0804a34860cd93cc5dc6fb0
Tom Janson <priv.tom.janson@gmail.com>
2017-12-12 19:23:53 +0100

    Update firefox-esr52.rb

--- Casks/firefox-esr52.rb
+++ Casks/firefox-esr52.rb
@@ -1,17 +1,18 @@
 cask 'firefox-esr52' do
-  version '52.1.0'
-  sha256 'c6f8f07bcab2782825538bbfc34eb46396b14e67f95993e7b06555d6b8b6b9a6'
+  version '52.5.0'
+  sha256 '0b8a1a3f3ba181537042a5ecfd91427d2711a086a2c5cef63c0a4079949117c2'
 
   # mozilla.net was verified as official when first introduced to the cask
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/en-US/Firefox%20#{version}esr.dmg"
-  name 'Mozilla Firefox'
+  name 'Mozilla Firefox ESR'
+  name 'Mozilla Firefox Extended Support Release'
   homepage 'https://www.mozilla.org/en-US/firefox/organizations/'
 
   license :mpl
 
   app 'Firefox.app'
 
-  zap delete: [
+  zap trash: [
                 '~/Library/Application Support/Firefox',
                 '~/Library/Caches/Firefox',
               ]
